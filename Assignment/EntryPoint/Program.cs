e7afa193379c5dcd164dc452fce4fbff73d1fec1
Daan Grashoff <daan_donn@hotmail.com>
2017-01-30 16:19:30 +0100

    assignment

---
+++ Assignment/EntryPoint/Program.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EntryPoint;
+
+namespace EntryPoint
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        static void CrashMe(int n)
+        {
+            Console.Write("Going strong at level " + n + "\r                       ");
+            // CrashMe(n + 1);
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            var fullscreen = false;
+
+            //Console.WriteLine("Which assignment shall run next? (1, 2, 3, 4, or q for quit) Choose assignment:");
+
+            switch ("3")
+            {
+                case "1":
+                    var game1 = VirtualCity.RunAssignment1(SortSpecialBuildingsByDistance, fullscreen);
+                    game1.Run();
+                    break;
+                case "2":
+                    var game2 = VirtualCity.RunAssignment2(FindSpecialBuildingsWithinDistanceFromHouse, fullscreen);
+                    game2.Run();
+                    break;
+                case "3":
+                    var game3 = VirtualCity.RunAssignment3(FindRoute, fullscreen);
+                    game3.Run();
+                    break;
+                case "4":
+                    var game4 = VirtualCity.RunAssignment4(FindRoutesToAll, fullscreen);
+                    game4.Run();
+                    break;
+                case "q":
+                    return;
+                default:
+                    var game12 = VirtualCity.RunAssignment1(SortSpecialBuildingsByDistance, fullscreen);
+                    game12.Run();
+                    break;
+            }
+
+        }
+
+        private static IEnumerable<Vector2> SortSpecialBuildingsByDistance(Vector2 house, IEnumerable<Vector2> specialBuildings)
+        {
+            var SpecialBuildings = specialBuildings.ToArray();
+            Sorting.MergeSortByHouse(SpecialBuildings, 0, SpecialBuildings.Length - 1, house);
+
+            Console.WriteLine("Test");
+            return SpecialBuildings;
+        }
+
+        private static IEnumerable<IEnumerable<Vector2>> FindSpecialBuildingsWithinDistanceFromHouse(
+          IEnumerable<Vector2> specialBuildings,
+          IEnumerable<Tuple<Vector2, float>> housesAndDistances)
+        {
+            var Specialbuildings = specialBuildings.ToArray();
+
+            var kdTree = Sorting.BuildKdTree(Specialbuildings, 0);
+            //var kdTree2 = Sorting.BuildKdTree2(Specialbuildings, 0, Specialbuildings.Length - 1, 0);
+
+            Sorting.TreeWalker(kdTree);
+            var houses = new List<Vector2>();
+            foreach (var house in housesAndDistances)
+            {
+                Sorting.TreeRange(kdTree, house, 0, houses);
+            }
+
+            var list = new List<List<Vector2>>();
+            list.Add(houses);
+            return list;
+        }
+
+        class vertice
+        {
+            public bool Visited { get; set; }
+            public float Distance { get; set; }
+        }
+
+
+        private static IEnumerable<Tuple<Vector2, Vector2>> FindRoute(Vector2 startingBuilding,
+          Vector2 destinationBuilding, IEnumerable<Tuple<Vector2, Vector2>> roads)
+        {
+            var adjList = roads.GroupBy(road => road.Item1).ToDictionary(group => group.Key, group => group.Select(t => t.Item2));
+            var NodeDistance = roads.GroupBy(road => road.Item1).ToDictionary(group => group.Key, group => new vertice { Visited = false, Distance = float.PositiveInfinity });
+
+
+
+
+
+
+            var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+            List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+            var prevRoad = startingRoad;
+            for (int i = 0; i < 300; i++)
+            {
+                prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, destinationBuilding)).First());
+                fakeBestPath.Add(prevRoad);
+            }
+
+
+            return fakeBestPath;
+        }
+
+        private static IEnumerable<IEnumerable<Tuple<Vector2, Vector2>>> FindRoutesToAll(Vector2 startingBuilding,
+          IEnumerable<Vector2> destinationBuildings, IEnumerable<Tuple<Vector2, Vector2>> roads)
+        {
+            List<List<Tuple<Vector2, Vector2>>> result = new List<List<Tuple<Vector2, Vector2>>>();
+            foreach (var d in destinationBuildings)
+            {
+                var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+                List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+                var prevRoad = startingRoad;
+                for (int i = 0; i < 30; i++)
+                {
+                    prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, d)).First());
+                    fakeBestPath.Add(prevRoad);
+                }
+
+
+                result.Add(fakeBestPath);
+            }
+            return result;
+        }
+    }
+#endif
+}
---
+++ Assignment/EntryPoint/Sorting.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace EntryPoint
+{
+	public class Sorting
+	{
+		public static void MergeSortByHouse(Vector2[] B, int p, int r, Vector2 house)
+		{
+			if (p < r)
+			{
+				int q = (p + r) / 2;
+
+				MergeSortByHouse(B, p, q, house);
+				MergeSortByHouse(B, q + 1, r, house);
+
+			}
+
+		}
+
+		public static void MergeByHouse(Vector2[] B, int p, int q, int r, Vector2 house)
+		{
+			int n1 = q - p + 1;
+			int n2 = r - q;
+
+			var L = new Vector2[n1];
+			var R = new Vector2[n2];
+
+			for (int x = 0; x < n1; x++)
+			{
+				L[x] = B[p + x];
+			}
+
+			for (int y = 0; y < n2; y++)
+			{
+				R[y] = B[q + y + 1];
+			}
+
+			int i = 0;
+			int j = 0;
+			int k = p;
+
+			while (i < n1 && j < n2)
+			{
+				if (Distance(L[i], house) < Distance(R[j], house))
+				{
+					B[k] = L[i];
+					i++;
+				}
+				else {
+					B[k] = R[j];
+					j++;
+				}
+				k++;
+			}
+
+			while (i < n1)
+			{
+				B[k] = L[i];
+				i++;
+				k++;
+			}
+
+			while (j < n2)
+			{
+				B[k] = R[j];
+				j++;
+				k++;
+			}
+		}
+
+
+		public static float Distance(Vector2 a, Vector2 b)
+		{
+			return (float)Math.Sqrt(Math.Pow(a.X - b.X,2) + Math.Pow(a.Y - b.Y,2));
+		}
+
+		public static void MergeSort(Vector2[] points, int p, int r, int depth)
+		{
+			if (p < r)
+			{
+				int q = (p + r) / 2;
+				MergeSort(points, p, q, depth);
+				MergeSort(points, q + 1, r, depth);
+				Merge(points, p, q, r, depth);
+			}
+		}
+
+		public static void Merge(Vector2[] points, int p, int q, int r, int depth)
+		{
+			int n1 = q - p + 1;
+			int n2 = r - q;
+
+			Vector2[] L = new Vector2[n1];
+			Vector2[] R = new Vector2[n2];
+
+			for (int i = 0; i < n1; i++)
+				L[i] = points[p + i];
+
+			for (int j = 0; j < n2; j++)
+				R[j] = points[j + q + 1];
+
+			int ii = 0;
+			int jj = 0;
+			int k = p;
+
+			while (ii < n1 && jj < n2)
+			{
+				if (depth % 2 == 0)
+				{
+					if (L[ii].X < R[jj].X)
+					{
+						points[k] = L[ii];
+						ii++;
+					}
+					else
+					{
+						points[k] = R[jj];
+						jj++;
+					}
+				}
+				else {
+					if (L[ii].Y < R[jj].Y)
+					{
+						points[k] = L[ii];
+						ii++;
+					}
+					else
+					{
+						points[k] = R[jj];
+						jj++;
+					}
+				}
+				k++;
+			}
+
+			while (ii < n1)
+			{
+				points[k] = L[ii];
+				ii++;
+				k++;
+			}
+			while (jj < n2)
+			{
+				points[k] = R[jj];
+				jj++;
+				k++;
+			}
+		}
+
+		public static void TreeWalker(ITree<Vector2> T)
+		{
+			if (!T.IsEmpty)
+			{
+				//L.Add(T.Value);
+				Console.WriteLine(T.Value);
+				TreeWalker(T.Left);
+				TreeWalker(T.Right);
+			}
+		}
+
+
+		public static void TreeRange(ITree<Vector2> T, Tuple<Vector2, float> hd, int depth, List<Vector2> L)
+		{
+			if (!T.IsEmpty)
+			{
+				var Xmin = hd.Item1.X - hd.Item2;
+				var Xmax = hd.Item1.X + hd.Item2;
+
+				var Ymin = hd.Item1.Y - hd.Item2;
+				var Ymax = hd.Item1.Y + hd.Item2;
+
+				if (T.Value.X > Xmin && T.Value.X < Xmax && T.Value.Y > Ymin && T.Value.Y < Ymax)
+				{
+					if(Distance(T.Value, hd.Item1) <= hd.Item2)
+						L.Add(T.Value);
+				}
+
+				bool axis = depth % 2 == 0;
+
+				if (T.Value.X >= Xmin && T.Value.X <= Xmax && axis ||
+					T.Value.Y >= Ymin && T.Value.Y <= Ymax && !axis)
+				{
+					TreeRange(T.Left, hd, depth + 1, L);
+					TreeRange(T.Right, hd, depth + 1, L);
+				}
+				else if (T.Value.X < Xmin && axis ||
+						 T.Value.Y < Ymin && !axis)
+				{
+					TreeRange(T.Right, hd, depth + 1, L);
+				}
+				else if (T.Value.X > Xmax && axis ||
+						 T.Value.Y > Ymax && !axis)
+				{
+					TreeRange(T.Left, hd, depth + 1, L);
+				}
+			}
+		}
+
+		public static ITree<Vector2> BuildKdTree(Vector2[] P, int depth)
+		{
+			Sorting.MergeSort(P, 0, P.Length - 1, depth);
+			int v = P.Length / 2;
+			
+			Vector2[] L = P.Take(P.Length / 2).ToArray();
+			Vector2[] R = P.Skip(P.Length / 2 + 1).ToArray();
+
+			if (P.Length == 1)
+			{
+				return new Node<Vector2>(new Empty<Vector2>(), P[0], new Empty<Vector2>());
+			}
+			ITree<Vector2> root;
+			if (P.Length == 0)
+				root = new Empty<Vector2>();
+			else {
+				root = new Node<Vector2>(new Empty<Vector2>(), P[v], new Empty<Vector2>());
+				root.Left = BuildKdTree(L, depth + 1);
+				root.Right = BuildKdTree(R, depth + 1);
+			}
+
+			return root;
+		}
+
+		public static ITree<Vector2> BuildKdTree2(Vector2[] P, int p, int r, int depth)
+		{
+			Sorting.MergeSort(P, p, r, depth);
+			int median = P.Length / 2;
+
+			ITree<Vector2> root;
+			if (r - p == 1)
+			{
+				return new Node<Vector2>(new Empty<Vector2>(), P[p], new Empty<Vector2>());
+			}
+			if (r < p)
+			{
+				root = new Empty<Vector2>();
+			}
+			else {
+				root = new Node<Vector2>(new Empty<Vector2>(), P[median], new Empty<Vector2>());
+				root.Left = BuildKdTree2(P, p, median - 1, depth - 1);
+				root.Right = BuildKdTree2(P, median + 1, r, depth - 1);
+			}
+		
+			return root;
+		}
+	}
+
+
+	public interface ITree<T>
+	{
+		bool IsEmpty { get; }
+		ITree<T> Left { get; set; }
+		ITree<T> Right { get; set; }
+		T Value { get; }
+	}
+
+	public class Empty<T> : ITree<T>
+	{
+		public bool IsEmpty
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public ITree<T> Left
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public ITree<T> Right
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public T Value
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		ITree<T> ITree<T>.Left
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		ITree<T> ITree<T>.Right
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+
+	public class Node<T> : ITree<T>
+	{
+		public bool IsEmpty
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		public ITree<T> Left
+		{
+			get;set;
+		}
+
+		public ITree<T> Right
+		{
+			get;set;
+		}
+
+		public T Value
+		{
+			get;set;
+		}
+
+		public Node(ITree<T> l, T v, ITree<T> r)
+		{
+			this.Left = l;
+			this.Value = v;
+			this.Right = r;
+		}
+	}
+
+}
